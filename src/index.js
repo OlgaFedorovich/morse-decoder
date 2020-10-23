56384e84fd89208b1793b840f469488df2434944
OlgaFedorovich <63749183+OlgaFedorovich@users.noreply.github.com>
2020-10-23 23:48:25 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,44 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    'space':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let letterCode;
+    let array = [];
+    let k = 0;
+    let str = '';
+    let array2 = [];
+    for( let i = 1; i <= expr.length / 10; i++ ) {
+
+        letterCode = expr.slice(k, (i*10));
+        array.push(letterCode);
+        k = k + 10;
+    }
+    console.log(array);
+
+    array.forEach(function(string, index) {
+        str = '';
+        for (let i = 0; i < string.length; i++) {
+            
+            if(string[i] === '0') continue;
+            if(string[i] === '1' && string[i+1] === '0') {
+                str = str + '.';
+            }
+            if(string[i] === '1' && string[i+1] === '1') {
+                str = str + '-';
+            }
+            if(string[i] === '*') {
+                str = 'space';
+            }
+            i++;
+        }
+        
+        array2.push(MORSE_TABLE[`${str}`]);
+    });
+    console.log(array2.join(''));
+    return array2.join('');
 }
 
 module.exports = {
